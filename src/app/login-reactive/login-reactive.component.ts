6e5edc39416236cb9c6be79947f7ff6ad995a3c7
shakib-hassan <shakib.hassan@iqvia.com>
2024-07-02 20:31:30 +0600

    'add_reactive_form'

---
+++ src/app/login-reactive/login-reactive.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { createPasswordStrengthValidator } from "../validators/password-strength.validator";
+
+@Component({
+  selector: "login",
+  templateUrl: "./login-reactive.component.html",
+  styleUrls: ["./login-reactive.component.css"],
+})
+export class LoginReactiveComponent implements OnInit {
+  email = new FormControl("", {
+    validators: [Validators.required, Validators.email],
+    updateOn: "blur",
+  });
+  password = new FormControl("", {
+    validators: [
+      Validators.required,
+      Validators.minLength(8),
+      createPasswordStrengthValidator(),
+    ],
+  });
+  reactive_form = new FormGroup({
+    email: this.email,
+    password: this.password,
+  });
+  constructor() {}
+
+  ngOnInit() {}
+}
