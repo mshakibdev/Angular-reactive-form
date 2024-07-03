19373fc9063c0dc13bb2fa76ba0d865665254112
shakib(github) <mshassan.dhk313@gmail.com>
2024-07-03 17:50:04 +0600

    step 1 form building

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,18 +1,22 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {CoursesService} from '../../services/courses.service';
-import {Observable} from 'rxjs';
-import {filter} from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { CoursesService } from "../../services/courses.service";
+import { Observable } from "rxjs";
+import { filter } from "rxjs/operators";
 
 @Component({
-  selector: 'create-course-step-1',
-  templateUrl: './create-course-step-1.component.html',
-  styleUrls: ['./create-course-step-1.component.scss']
+  selector: "create-course-step-1",
+  templateUrl: "./create-course-step-1.component.html",
+  styleUrls: ["./create-course-step-1.component.scss"],
 })
 export class CreateCourseStep1Component implements OnInit {
+  form = this.formBuilder.group({
+    title: [
+      "",
+      [Validators.required, Validators.minLength(5), Validators.maxLength(40)],
+    ],
+  });
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit() {
-
-  }
-
+  ngOnInit() {}
 }
