8843c73445d95d1baac829df0b136484982e47cd
shakib(github) <mshassan.dhk313@gmail.com>
2024-07-03 14:56:35 +0600

    added starter file

--- src/app/create-course/create-course.component.ts
+++ src/app/create-course/create-course.component.ts
@@ -7,12 +7,7 @@ import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 @Component({
   selector: 'create-course',
   templateUrl: './create-course.component.html',
-  styleUrls: ['./create-course.component.scss'],
-  providers: [
-      {
-          provide: STEPPER_GLOBAL_OPTIONS, useValue: {showError:true}
-      }
-  ]
+  styleUrls: ['./create-course.component.scss']
 })
 export class CreateCourseComponent implements OnInit {
 
@@ -20,12 +15,5 @@ export class CreateCourseComponent implements OnInit {
   ngOnInit() {
 
   }
-
-
-    submit(step1, step2, step3) {
-
-      console.log(step1, step2, step3);
-
-    }
 
 }
