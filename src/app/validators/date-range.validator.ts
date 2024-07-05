371f36c333588bf2e097635619ac825ecb5a1172
shakib(github) <mshassan.dhk313@gmail.com>
2024-07-05 12:55:04 +0600

    add a date range validators

--- src/app/validators/date-range.validator.ts
+++ src/app/validators/date-range.validator.ts
@@ -1,19 +1,17 @@
-import {FormGroup, ValidatorFn, Validators} from '@angular/forms';
-
+import { FormGroup, ValidatorFn, Validators } from "@angular/forms";
 
 export function createPromoRangeValidator(): ValidatorFn {
-    return (form: FormGroup): Validators | null => {
+  return (form: FormGroup): Validators | null => {
+    const start: Date = form.get("promoStartAt").value;
 
-        const start:Date = form.get("promoStartAt").value;
+    const end: Date = form.get("promoEndAt").value;
 
-        const end:Date = form.get("promoEndAt").value;
+    if (start && end) {
+      const isRangeValid = end.getTime() - start.getTime() > 0;
 
-        if (start && end) {
-            const isRangeValid = (end.getTime() - start.getTime() > 0);
-
-            return isRangeValid ? null : {promoPeriod:true};
-        }
-
-        return null;
+      return isRangeValid ? null : { promoPeriod: true };
     }
+
+    return null;
+  };
 }
